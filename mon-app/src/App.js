83e1b1e75bf1d8f62d48fbe26057bc3e62136451
Fabien <84633381+Zearcus@users.noreply.github.com>
2022-02-28 18:23:08 +0100

    add acceuil and modif app

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom'
+
+import Acceuil from './Acceuil';
+import Page1 from './Page1';
 import './App.css';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Acceuil />} />
+        <Route exact path='/Page1' element={<Page1 />} />
+      </Routes>
+    </Router>
   );
 }
 
